9e1e12ad05928608bfb2a35a1a762f1363545266
Desislava <desislava.tencheva@outlook.com>
2022-10-23 13:33:28 +0300

    init project

---
+++ src/main/java/com/dreekde/sportal/SportalApplication.java
@@ -0,0 +1,13 @@
+package com.dreekde.sportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportalApplication.class, args);
+    }
+
+}
